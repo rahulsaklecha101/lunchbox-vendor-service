429e660563c523ca86e4bb1315e99139b4a2a3a9
Rahul Saklecha <rahul_saklecha@persistent.com>
2021-07-24 02:54:47 +0530

    project skeleton

---
+++ src/test/java/com/lunchbox/vendor/VendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunchbox.vendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
